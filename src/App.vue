0e46fcc9f3f07ce4b2430b0b75e4cf71930d3e83
Auvic <victor.trani1@gmail.com>
2022-05-14 13:56:44 -0300

    base vue structure created

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <ComponentHeader />
+    <ComponentMain />
+  </div>
+</template>
+
+<script>
+import ComponentHeader from "./components/ComponentHeader.vue";
+import ComponentMain from "./components/ComponentMain.vue";
+
+
+
+export default {
+  name: "App",
+  components: {
+    ComponentHeader,
+    ComponentMain
+  }
+}
+
+</script>
+
+<style>
+/* The reset css code below */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  font-family: 'Fira Sans', sans-serif;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
