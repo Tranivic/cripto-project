ac786bbc8e855718fa13908bf4f24bb0ee80730e
Victor <victor.trani1@gmail.com>
2022-11-02 18:42:49 -0300

    .env file hidding api key implemented

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,45 @@
 <template>
-
   <div class="app">
     <!-- ========== Header ========== -->
-    <header class="font-black md:text-6xl sm:text-5xl text-stone-400 pt-20 justify-center flex">
+    <header
+      class="font-black md:text-6xl sm:text-5xl text-stone-400 pt-20 justify-center flex"
+    >
       <h1>THE<strong class="text-stone-800">CRIPTO</strong>DATA</h1>
     </header>
     <!-- ========== Main ========== -->
     <main>
-      <form class="pt-10 flex search-bar h-full flex-col text-center items-center" @submit.prevent="addCoin">
-        <input placeholder="Search for currency to add..."
-          class="bg-zinc-100 shadow-lg max-w-2xl w-3/5 py-3 pl-5 rounded-lg" type="search" required v-model="input"
-          @input="showDrop">
-        <ul v-show="dropList.length"
-          class="drop-list bg-stone-800 text-white shadow-lg max-w-2xl w-3/5 rounded-lg rounded-t-none text-left">
-          <DropDown v-for="drop in dropList" :key="drop.asset_id" :drop="drop" @drop-clicked="dropClicked" />
+      <form
+        class="pt-10 flex search-bar h-full flex-col text-center items-center"
+        @submit.prevent="addCoin"
+      >
+        <input
+          placeholder="Search for currency to add..."
+          class="bg-zinc-100 shadow-lg max-w-2xl w-3/5 py-3 pl-5 rounded-lg"
+          type="search"
+          required
+          v-model="input"
+          @input="showDrop"
+        />
+        <ul
+          v-show="dropList.length"
+          class="drop-list bg-stone-800 text-white shadow-lg max-w-2xl w-3/5 rounded-lg rounded-t-none text-left"
+        >
+          <DropDown
+            v-for="drop in dropList"
+            :key="drop.asset_id"
+            :drop="drop"
+            @drop-clicked="dropClicked"
+          />
         </ul>
       </form>
       <div class="notFindMsg flex justify-center">
-        <h1 v-show="this.hide == true" class="max-w-2xl w-3/5 text-red-500">Coin not found...</h1>
+        <h1 v-show="this.hide == true" class="max-w-2xl w-3/5 text-red-500">
+          Coin not found...
+        </h1>
       </div>
-      <ul class="justify-center cards-container gap-10 grid py-20 container mx-auto">
+      <ul
+        class="justify-center cards-container gap-10 grid py-20 container mx-auto"
+      >
         <template v-if="newCoins.length">
           <CoinCard v-for="coin in newCoins" :key="coin.name" :coin="coin" />
         </template>
@@ -33,7 +53,7 @@
 import { ref, onMounted } from "vue";
 import CoinCard from "./components/CoinCard.vue";
 import DropDown from "./components/DropDown.vue";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "App",
@@ -51,18 +71,20 @@ export default {
 
     const getCoins = () => {
       let formatedCoins = [];
-      axios.get(`https://rest.coinapi.io/v1/assets/?apikey=836C99F4-29ED-4AFC-AB22-A4DB3678C94B`)
-        .then(response => {
-          let myTarget = JSON.parse(JSON.stringify(response.data))
-          myTarget.forEach(element => {
-            if (element.id_icon && element.price_usd) {
-              formatedCoins.push(element);
-            }
-          })
-          coinList.value = formatedCoins;
-          formatCoins();
+      const API_REQUEST = process.env.VUE_APP_API_REQUEST;
+
+      axios.get(`${API_REQUEST}`).then((response) => {
+        let myTarget = JSON.parse(JSON.stringify(response.data));
+        myTarget.forEach((element) => {
+          if (element.id_icon && element.price_usd) {
+            formatedCoins.push(element);
+          }
         });
-    }
+        coinList.value = formatedCoins;
+        formatCoins();
+      });
+    };
+
     //Function to formated coins array pulled from api database
     const formatCoins = () => {
       coinList.value.sort(function (a, b) {
@@ -72,30 +94,21 @@ export default {
         element.name[0].toUpperCase() + element.name.slice(1).toLowerCase();
         element.id_icon = element.id_icon.replace(/-/g, "");
       });
-    }
-
-    //Function att coins prices every 5 minutes
-    const attPrices = () => {
-      // getCoins();
-      // let obj = {};
-      // if (newCoins.value.length > 0) {
-      //   newCoins.value.forEach((element) => {
-      //     pushNewCoin(obj, element)
-      //   });
-      //   console.log("Atualizei");
-      // }
     };
-    setInterval(attPrices, 5000);
+
     // Function to push coin to array list
     const pushNewCoin = () => {
-      let counter = 0
-      coinList.value.forEach(element => {
-        if (element.name.toUpperCase() == input.value.toUpperCase() || element.asset_id == input.value.toUpperCase()) {
+      let counter = 0;
+      coinList.value.forEach((element) => {
+        if (
+          element.name.toUpperCase() == input.value.toUpperCase() ||
+          element.asset_id == input.value.toUpperCase()
+        ) {
           counter = counter + 1;
           newCoins.value.push(element);
           dropList.value = [];
         }
-      })
+      });
       if (counter == 0) {
         hide.value = true;
         counter = 0;
@@ -121,7 +134,7 @@ export default {
 
       if (input.value.length > 1) {
         dropList.value = [];
-        coinList.value.forEach(element => {
+        coinList.value.forEach((element) => {
           if (element.name.toUpperCase().startsWith(inp)) {
             dropList.value.push(element);
           }
@@ -146,21 +159,20 @@ export default {
       showDrop,
       dropClicked,
       getCoins,
-      attPrices,
       formatCoins,
       dropList,
       input,
       hide,
       newCoins,
       coinList,
-    }
+    };
   },
 
-
-
-  components: { CoinCard, DropDown }
-}
-
+  components: {
+    CoinCard,
+    DropDown,
+  },
+};
 </script>
 
 <style>
@@ -170,7 +182,7 @@ export default {
   padding: 0;
   box-sizing: border-box;
 
-  font-family: 'Fira Sans', sans-serif;
+  font-family: "Fira Sans", sans-serif;
 }
 
 a {
